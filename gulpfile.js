3160f9368a77d660444ddd0a8614d5e5b6b50fb9
bkimbrel <bkimbrel2@gmail.com>
2017-08-24 20:34:03 -0600

    added gulp-sass to pkg json

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var path = require('path');
+
+gulp.task('sass', function () {
+  return gulp.src('./public/assets/sass/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./public/assets/css'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./public/assets/sass/*.scss', ['sass']);
+});
