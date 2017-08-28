8405700e67df69999ad035608168206419c0bcec
Jeff Carlow <jeff.carlow@gmail.com>
2017-08-27 20:21:04 -0600

    cookies

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -48,5 +48,31 @@ $(document).ready(function () {
             return tmp;
         }
     }
+    document.cookie =
+    'ppkcookie1=testcookie; expires=Wed, 28 Aug 2018 20:47:11 UTC; path=/selection'
 
+    function createCookie(name,value,days) {
+        if (days) {
+            var date = new Date();
+            date.setTime(date.getTime()+(days*24*60*60*1000));
+            var expires = "; expires="+date.toGMTString();
+        }
+        else var expires = "";
+        document.cookie = name+"="+value+expires+"; path=/";
+    }
+    
+    function readCookie(name) {
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(';');
+        for(var i=0;i < ca.length;i++) {
+            var c = ca[i];
+            while (c.charAt(0)==' ') c = c.substring(1,c.length);
+            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+        }
+        return null;
+    }
+    
+    function eraseCookie(name) {
+        createCookie(name,"",-1);
+    }
 });
