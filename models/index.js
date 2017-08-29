95a1bef8b1437585618a5ebde0c1586d82590e1c
Jeff Carlow <jeff.carlow@gmail.com>
2017-08-29 17:16:36 -0600

    change

--- models/index.js
+++ models/index.js
@@ -4,7 +4,7 @@ var fs        = require('fs');
 var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(module.filename);
-var env       = process.env.NODE_ENV || 'development';
+var env       = process.env.NODE_ENV || 'production';
 console.log('WHAT IS THE ENV?', env);
 var config    = require(__dirname + '/../config/config.json')[env];
 console.log(' WHAT IS THE CONFIG?',config);
